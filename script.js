f25798224040e8d7984857118021c1f77c395fd9
Morenike Oyewole <herroyalpianist@gmail.com>
2021-07-07 23:02:07 +0100

    .

--- script.js
+++ script.js
@@ -3,39 +3,28 @@
 
 //   const element = document.body;
 //   const container = document.querySelector(".courses-container");
-//   const peachBg = document.querySelector(".intro");
 //   const footer = document.querySelector(".footer");
+//   const darkBtn = document.querySelector(".fa-adjust");
+
 //   darkBtn.addEventListener("click", () => {
 //     element.classList.toggle("dark-mode-bg");
-//     peachBg.classList.toggle("dark-mode-bg2");
 //     footer.classList.toggle("dark-mode-bg2");
 //     container.classList.toggle("dark-mode-bg2");
 //   });
 // };
 // darkmode();
 
-const bgDark = () => {
-  const element = document.body;
-  const darkBtn = document.querySelector(".fa-adjust");
-  const peachBg = document.documentElement.style.fontVariant();
-  darkBtn.addEventListener("click", () => {
-    element.classList.toggle("dark-mode-bg");
-    if (condition) {
-    } else {
-    }
-    // document.documentElement.style.setProperty("--peach-bg", "#121212");
-  });
-};
-bgDark();
+// document.documentElement.style.setProperty("--peach-bg", "#121212");
 
-// // page toggler
+// page toggler
 // const tabs = () => {
-//   const firstPage = document.querySelectorAll("#html");
-//   const secondPage = document.querySelectorAll("#css");
-//   const thirdPage = document.querySelectorAll("#javascript");
-//   const htmlBtn = document.querySelector(".html");
-//   const cssBtn = document.querySelector(".css");
-//   const javascriptBtn = document.querySelector(".javascript");
+//   const firstPage = document.querySelector(".page1");
+//   const secondPage = document.querySelector(".page2");
+//   const thirdPage = document.querySelector(".page3");
+//   const htmlBtn = document.querySelector("#htmlbtn");
+//   const cssBtn = document.querySelector("#cssbtn");
+//   const javascriptBtn = document.querySelector("#jsbtn");
+
 //   cssBtn.addEventListener("click", () => {
 //     firstPage.style.display = "none";
 //     thirdPage.style.display = "none";
