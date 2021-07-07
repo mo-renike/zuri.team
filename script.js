860fc374c912b769761c59889f5548a3776fad38
Morenike Oyewole <herroyalpianist@gmail.com>
2021-07-07 11:57:37 +0100

    ..

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 const darkmode = () => {
   const darkBtn = document.querySelector(".fa-adjust");
   const element = document.body;
-  const container = document.querySelector('.courses-container')
+  const container = document.querySelector(".courses-container");
   const peachBg = document.querySelector(".intro");
   const footer = document.querySelector(".footer");
   darkBtn.addEventListener("click", () => {
@@ -13,3 +13,29 @@ const darkmode = () => {
   });
 };
 darkmode();
+
+// page toggler
+const tabs = () => {
+  const firstPage = document.querySelectorAll("#html");
+  const secondPage = document.querySelectorAll("#css");
+  const thirdPage = document.querySelectorAll("#javascript");
+  const html = document.querySelector(".html");
+  const css = document.querySelector(".css");
+  const javascript = document.querySelector(".javascript");
+  css.addEventListener("click", () => {
+    firstPage.style.display = "none";
+    thirdPage.style.display = "none";
+    secondPage.style.display = "block";
+  });
+  javascript.addEventListener("click", () => {
+    firstPage.style.display = "none";
+    thirdPage.style.display = "block";
+    secondPage.style.display = "none";
+  });
+  html.addEventListener("click", () => {
+    firstPage.style.display = "block";
+    thirdPage.style.display = "none";
+    secondPage.style.display = "none";
+  });
+};
+tabs();
